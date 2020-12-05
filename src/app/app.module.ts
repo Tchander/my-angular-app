44ce3bbcc2d78f2483921d982fea9fa51f4d6dfe
Tchander <evgeniy.tchander@yandex.ru>
2020-12-05 15:03:16 +0300

    Create form component with ng generate component and add font-style roboto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {CardComponent} from './card/card.component';
+import { CardComponent } from './card/card.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
