282e5f7f154d3339760984d558b3d22c8481b41d
Tchander <evgeniy.tchander@yandex.ru>
2020-12-05 16:33:21 +0300

    Add button changTitle method and input event with bindings

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,8 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
-export class CardComponent {}
+export class CardComponent implements OnInit {
+
+  title = 'My Card Title'
+  text: string = 'My sample text'
+
+  ngOnInit() {}
+
+  changeTitle() {
+    this.title = 'Title has been changed'
+  }
+
+  inputHandler(event: any) {
+    const value = event.target.value
+    this.title = value
+  }
+}
