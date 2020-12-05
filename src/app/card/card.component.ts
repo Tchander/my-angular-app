d4472c1303a35140a7371316a799f8edc0ca485c
Tchander <evgeniy.tchander@yandex.ru>
2020-12-05 18:27:07 +0300

    Add date localeRu and finish the Angular Basics by Vladilen Minin

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,21 +1,27 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Card} from '../app.component';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css'],
+  styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
 
+  @Input() card: Card
+  @Input() index: number
+
   title = 'My Card Title'
   text: string = 'My sample text'
+
+  cardDate: Date = new Date()
 
   textColor: string
 
   ngOnInit() {}
 
   changeTitle() {
-    this.title = 'Title has been changed'
+    this.card.title = 'Title has been changed'
   }
 
   inputHandler(value) {
